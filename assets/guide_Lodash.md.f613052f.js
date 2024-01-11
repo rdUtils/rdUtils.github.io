a85f0cf844b1a7d693811699e975d2eb3815ec2b
yibin.lin <yibin.lin@tcl.com>
2024-01-11 20:04:08 +0800

    feat: 部署

---
+++ assets/guide_Lodash.md.f613052f.js
@@ -0,0 +1,28 @@
+import{_ as s,o as a,c as n,a as l}from"./app.58811f42.js";const d=JSON.parse('{"title":"Lodash","description":"","frontmatter":{},"headers":[{"level":2,"title":"常用函数","slug":"常用函数","link":"#常用函数","children":[]}],"relativePath":"guide/Lodash.md","lastUpdated":1696675603000}'),p={name:"guide/Lodash.md"},o=l(`<h1 id="lodash" tabindex="-1">Lodash <a class="header-anchor" href="#lodash" aria-hidden="true">#</a></h1><h2 id="常用函数" tabindex="-1">常用函数 <a class="header-anchor" href="#常用函数" aria-hidden="true">#</a></h2><div class="info custom-block"><p class="custom-block-title">作用：</p><p>🎉 lodash常用函数</p></div><p><strong>Demo</strong></p><div class="language-typescript"><button title="Copy Code" class="copy"></button><span class="lang">typescript</span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#89DDFF;font-style:italic;">import</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">debounce</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">throttle</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">pick</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">omit</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">uniq</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">uniqBy</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">cloneDeep</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">noop</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">dropWhile</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">dropRightWhile</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">camelCase</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">capitalize</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">lowerCase</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">lowerFirst</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">isObject</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">isFunction</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">isString</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">isDate</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">isEqual</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">isEqualWith</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">eq</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">gt</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">gte</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">lt</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#BABED8;">lte</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;font-style:italic;">from</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">@rd/utils</span><span style="color:#89DDFF;">&#39;</span></span>
+<span class="line"></span></code></pre></div>`,5),e=[o];function t(c,r,D,i,y,F){return a(),n("div",null,e)}const h=s(p,[["render",t]]);export{d as __pageData,h as default};
